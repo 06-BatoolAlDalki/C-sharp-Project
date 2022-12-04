1416a6480f011dcf6d8e83964cb177ca72a3d66b
Mohammad-Sawalha98 <115452622+Mohammad-Sawalha98@users.noreply.github.com>
2022-12-04 10:26:44 -0800

    index

---
+++ C sharp project/C sharp project/addQuestions.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace C_sharp_project
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+            string gg = TextBox1.Text;
+            for (int y=1; y<=10; y++)
+            {
+                string[] text= new string[y];
+                text[y-1]= $"TextBox{[y].Text}";
+                Response.Write(text[y-1]);
+            }
+            //for(int i=1;i<=10;i++) 
+            //{
+            //    string[] print = new string[10] ;
+            //    print[i] = TextBox1.Text;
+            //}
+
+        }
+    }
+}
