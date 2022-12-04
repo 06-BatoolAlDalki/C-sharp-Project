7d422e7a3d87be2b75aec29b6ac09c007a191f23
Mo'ath Bdour <moathbdour@users.noreply.github.com>
2022-12-04 13:34:47 +0300

    new

--- C sharp project/C sharp project/addQuestions.aspx.cs
+++ C sharp project/C sharp project/addQuestions.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -17,23 +18,14 @@ namespace C_sharp_project
         protected void Button1_Click1(object sender, EventArgs e)
         {
 
-
-            int countTB = 0;
-            foreach (Control c in form1.Controls) //here is the minor change
-            {
-                if (c.GetType() == typeof(TextBox))
-                {
-                    countTB++;
-                }
-            }
-          Response.Write(countTB.ToString());
-            for(int i = 1; i <= 10; i++)
-            {
-                string[] moath= new string[countTB];
-                moath[i-1]= (TextBox)(TextBox[i]).Text;
-            }
+            string[] questions = String.Join(",", Form.Controls.OfType<TextBox>().Select(c => c.Text)).Split(',');
 
             
+          
+
+
+
+
         }
 
         protected void Button2_Click(object sender, EventArgs e)
